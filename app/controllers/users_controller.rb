251aff662ddf02166cc79c7bd7ac8ca6c0639e5f
tanduc <hotanduc.cs@gmail.com>
2023-07-19 11:54:09 +0700

    upadate infinite scroll

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -118,6 +118,7 @@ class UsersController < ApplicationController
 
 
     respond_to do |format|
+      sleep(0.5)
       format.html
       format.turbo_stream
     end
--- app/javascript/controllers/feedsPhoto_controller.js
+++ app/javascript/controllers/feedsPhoto_controller.js
@@ -5,12 +5,12 @@ export default class extends Controller {
         this.preview()
     }
     preview () {
+        // Add click event to show detail images
         const items = document.querySelectorAll(".list-items__item .card .item__images img")
         const myModal = new bootstrap.Modal(document.querySelector('.feeds-photo__model'), {})
         const imageOfModal = document.querySelector('.feeds-photo__model .modal-body img')
         const myModalTitle = document.querySelector('.feeds-photo__model .modal-header h1')
         const myModalDescription = document.querySelector('.feeds-photo__model .modal-body p')
-
         items.forEach(item => {
             item.addEventListener('click', (event) => {
                 const cardBody = item.closest('.card').querySelector('.card-body')
@@ -23,5 +23,16 @@ export default class extends Controller {
                 myModal.show();
             })
         })
+
+        //Loading animation
+        document.addEventListener('turbo:before-fetch-request', function() {
+            const spinner = document.getElementById('feeds_photo_loading');
+            spinner.style.display = 'block';
+        });
+
+        document.addEventListener('turbo:before-fetch-response', function() {
+            const spinner = document.getElementById('feeds_photo_loading');
+            spinner.style.display = 'none';
+        });
     }
 }
