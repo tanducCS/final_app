7af11276cfca83770ff1afc7f74c49878705a4aa
tanduc <hotanduc.cs@gmail.com>
2023-07-19 10:59:43 +0700

    add infinite scroll for feeds page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -114,6 +114,13 @@ class UsersController < ApplicationController
 
   def feeds_photos
     @followees = current_user.followees
+    @pagy, @photos = pagy_countless(Photo.where(user_id: @followees.ids), items: 10)
+
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
   def feeds_albums
     @followees = current_user.followees
